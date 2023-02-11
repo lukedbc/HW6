90dfa1dc17aff96f355db74c8449f5a4a81c4a1c
LukeD <locdaohuuit@outlook.com>
2023-02-11 01:01:13 -0800

    move HW5 to HW6

---
+++ main.js
@@ -0,0 +1,101 @@
+const signUpStorage = new Storage("sign-up-data", JSON.parse(getValueFromCache("sign-up-data")));
+const contactStorage = new Storage("contact-data", JSON.parse(getValueFromCache("contact-data")));
+
+function handleSignUp(e) {
+    handleInputTemplate(e, function(_) {
+        let signUpEntity = new SignUpEntity({
+            _studentId: getValueFromInputElement("sID"),
+            _firstName: getValueFromInputElement("firstName"),
+            _lastName: getValueFromInputElement("lastName"),
+            _gender: getValueFromInputElement("gender"),
+            _dob: getValueFromInputElement("birthday"),
+            _experience: getValueFromInputElement("background"),
+            _reason: getValueFromInputElement("reason"),
+            _other: getValueFromInputElement("extra"),
+        });
+
+        if (signUpEntity.isValid()) {
+            signUpStorage.add(signUpEntity);
+            alert("Sign up sucessfully!");
+            signUpStorage.saveToCache();
+        }
+    })
+}
+
+function handleContact(e) {
+    handleInputTemplate(e, function(_) {
+        let contact = new ContactEntity({
+            _name: getValueFromInputElement("name"),
+            _email: getValueFromInputElement("email"),
+            _message: getValueFromInputElement("message")
+        });
+
+        if (contact.isValid()) {
+            contactStorage.add(contact);
+            alert("Received your message!")
+            contactStorage.saveToCache();
+        }
+    })
+}
+
+function handleSignUpListBtn(_) {
+    show("sign-up-list", signUpStorage, function(model) {
+        return `${model.m_createdDate}: Name - ${model.m_firstName} ${model.m_lastName}, DOB: ${model.m_dob}`;
+    });
+}
+
+function handleContactListBtn(_) {
+    show("contact-list", contactStorage, function(model) {
+        return `${model.m_createdDate} - From ${model.m_name} - ${model.m_email}: ${model.m_message}`;
+    });
+}
+
+function show(elementId, storage, toStringFunction) {
+    storage.syncWithCache();
+    let rootElement = getElement(elementId);
+    rootElement.innerHTML = "";
+
+    const data = storage.getAll();
+    data.forEach(function(dataAsString, index) {
+        let liElement = document.createElement("li");
+        liElement.setAttribute("id", elementId + "-" + index);
+        liElement.innerText = toStringFunction(dataAsString);
+
+        rootElement.appendChild(liElement);
+    });
+}
+
+function handleInputTemplate(e, handleFunction) {
+    e.preventDefault();
+    try {
+        handleFunction(e);
+    } catch (e) {
+        console.log(e);
+        alert(e.message);
+    }
+}
+
+function initMatrix() {
+    drawMatrixTo("join-a-team-content");
+    for (let memberOrder = 1; memberOrder <= NUMBER_MEMEBER_EACH_TEAM; memberOrder++) {
+        for (let team = 1; team <= NUMBER_OF_TEAM; team++) {
+            addEventToSlot({
+                _team: team,
+                _memberOrder: memberOrder,
+                _handleFunction:
+                    function() {
+                        alert(`cick on slot: team-${team}-member-${memberOrder}`)
+                    }
+            })
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(_) {
+    initMatrix();
+    getElement("sign-up-btn").addEventListener("click", handleSignUp);
+    getElement("contact-us-btn").addEventListener("click", handleContact);
+
+    getElement("sign-up-list-btn").addEventListener("click", handleSignUpListBtn);
+    getElement("contact-list-btn").addEventListener("click", handleContactListBtn);
+});
---
+++ matrix.js
@@ -0,0 +1,50 @@
+const NUMBER_OF_TEAM = 3;
+const NUMBER_MEMEBER_EACH_TEAM = 5;
+
+function drawMatrixTo(elementId) {
+    let element = getElement(elementId);
+    let table = document.createElement("table");
+    table.setAttribute("id", "spreadsheet");
+
+    // draw team row
+    let teamRow = document.createElement("tr");
+    teamRow.setAttribute("id", "row-team");
+    for (let i = 1; i <= NUMBER_OF_TEAM; i++) {
+        let th = document.createElement("th");
+        th.setAttribute("id", "team-" + i);
+        th.innerText = "Team " + i;
+
+        teamRow.appendChild(th);
+    }
+    table.appendChild(teamRow);
+
+    for (let i = 1; i <= NUMBER_MEMEBER_EACH_TEAM; i++) {
+        let row = document.createElement("tr");
+        for (let j = 1; j <= NUMBER_OF_TEAM; j++) {
+            let td = document.createElement("td");
+            td.setAttribute("id", "team-" + j + "-member-" + i);
+
+            td.innerText = "Available to join";
+            row.appendChild(td);
+        }
+
+
+        table.appendChild(row);
+    }
+
+    element.appendChild(table);
+}
+
+function addEventToSlot({
+    _team,
+    _memberOrder,
+    _handleFunction
+}) {
+    let slot = getElement("team-" + _team + "-member-" + _memberOrder);
+
+    if (!slot) {
+        throw new Error("Slot not found");
+    }
+
+    slot.addEventListener("click", _handleFunction);
+}
